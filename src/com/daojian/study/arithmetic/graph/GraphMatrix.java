e26583a728121391147e1a4d9094ccb534f6c899
wangdaojian <419550295@qq.com>
2018-12-01 12:20:45 +0800

    添加图信息

--- src/com/daojian/study/arithmetic/graph/GraphMatrix.java
+++ src/com/daojian/study/arithmetic/graph/GraphMatrix.java
@@ -14,8 +14,11 @@ import com.daojian.study.arithmetic.graph.Vertex.VStatus;
  */
 public class GraphMatrix<Tv, Te>{
 
-	private Vector<Vertex<Tv>> V; //点集合
-	private Vector<Vector<Edge<Te>>> E; //边集
+	private Vector<Vertex<Tv>> V = new Vector<>(); //点集合
+	
+	private Vector<Vector<Edge<Te>>> E = new Vector<Vector<Edge<Te>>>(); //边集
+	
+	private int e; //边数
 	
 	/**
 	 * 第一个邻居
@@ -71,6 +74,63 @@ public class GraphMatrix<Tv, Te>{
 		if(exists(i, j)) return;
 		Edge<Te> te = new Edge<Te>(edge, weigth);
 		E.get(i).set(j, te);
+		e++;
+		V.get(i).outDegree++;
+		V.get(j).inDegree++;
+	}
+	/**
+	 * 删除边
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	Te removeEdge(int i, int j) {
+		Te eBak = E.get(i).get(j).data;
+		e--;
+		V.get(i).outDegree--;
+		V.get(j).inDegree--;
+		return eBak;
 		
 	}
+	/**
+	 * 插入点
+	 * @param tv
+	 */
+	void insertVertex(Tv tv) {
+		for(int j=0; j<V.size(); j++) {
+			E.get(j).add(null);
+		}
+		V.add(new Vertex<>(tv));
+		Vector<Edge<Te>> e = new Vector<>();
+		for(int j=0; j<V.size(); j++) {
+			e.add(null);
+		}
+		E.add(e);
+	}
+	
+	/**
+	 * 删除某顶点，及其关联边，返回顶点信息
+	 * @param i
+	 * @return
+	 */
+	Tv removeVertex(int i) {
+		//删除所有出边
+		for(int j=0; j<V.size(); j++) {
+			if(exists(i, j)) 
+				V.get(j).inDegree--;
+		}
+		
+		E.remove(i);
+		//备份该顶点，然后删除
+		Tv eBak = V.get(i).data;
+		V.remove(i);
+		
+		for(int j=0; j<V.size(); j++) {
+			if(E.get(j).remove(i) != null) {
+				V.get(j).outDegree--;
+			}
+		}
+		return eBak;
+	}
+	
 }
--- src/com/daojian/study/test/Test.java
+++ src/com/daojian/study/test/Test.java
@@ -1,17 +1,14 @@
 package com.daojian.study.test;
 
+import java.util.Vector;
+
 public class Test {
 	
 	enum EStatus{UNDETERMINED, TREE, CROSS, FORWARD, BACKWARD}
 
 	public static void main(String[] args) {
-		System.out.println(EStatus.UNDETERMINED.ordinal());
-		int[][] arr = new int[3][5];
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<5; j++) {
-				System.out.print(arr[i][j] + ", ");
-			}
-			System.out.println();
-		}
+		Vector<Vector<Integer>> v = new Vector<>();
+		v.get(1);
+		
 	}
 }
